8114048c3a77180cd9e5ec85bd71c93e47e6c004
Ashuhauhan <ashuc306@gmail.com>
2022-11-11 03:19:22 +0530

    case_digit_to_number

--- single_digit_to_word.sh
+++ single_digit_to_word.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
-read -p "Enter any single digit number :   " n;
-if [ $n -eq 1 ]
-then 
-    echo " one "
-elif [ $n -eq 2 ]
-then
-    echo " two "
-elif [ $n -eq 3 ]
-then
-    echo " three "
-elif [ $n -eq 4 ]
-then
-    echo " four "
-elif [ $n -eq 5 ]
-then
-    echo " five "
-elif [ $n -eq 6 ]
-then
-    echo " six "
-elif [ $n -eq 7 ]
-then
-    echo " seven "
-elif [ $n -eq 8 ]
-then
-    echo " eight "
-else
-    echo " nine "
-fi
+read -p "Enter any single digit number :   " digit;
+case $digit in
+1)
+ echo " one "
+;;
+2)
+ echo " two "
+;;
+3)
+ echo " three "
+;;
+4)
+ echo " four "
+;;
+5)
+ echo " five "
+;;
+6)
+ echo " six "
+;;
+7)
+ echo " seven "
+;;
+8)
+ echo " eight "
+;;
+9)
+ echo " nine "
+;;
+
+esac
